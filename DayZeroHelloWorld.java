c7fb967c7cbe00471d7f93fbefa438f0b8aab8fa
BrinzN <svn.brindha@gmail.com>
2016-11-01 17:52:55 -0500

    Update DayZeroHelloWorld.java

--- DayZeroHelloWorld.java
+++ DayZeroHelloWorld.java
@@ -1,5 +1,19 @@
 import java.util.Scanner;
 
+/*
+Input Format
+A single line of text denoting  (the variable whose contents must be printed).
+
+Output Format
+Print Hello, World. on the first line, and the contents of  on the second line.
+
+Sample Input
+Welcome to 30 Days of Code!
+
+Sample Output
+Hello, World. 
+Welcome to 30 Days of Code!*/
+
 public class DayZeroHelloWorld {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
