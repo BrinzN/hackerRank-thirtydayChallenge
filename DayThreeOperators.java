d3e02ea7ab2abd1906aa6e39d77ab49ae5692934
BrinzN <svn.brindha@gmail.com>
2016-11-02 10:11:38 -0500

    Create DayThreeOperators.java

---
+++ DayThreeOperators.java
@@ -0,0 +1,21 @@
+/*
+Input Format
+There are 3 lines of numeric input: 
+The first line has a double, mealCost (the cost of the meal before tax and tip). 
+The second line has an integer, tipPercent (the percentage of mealCost being added as tip). 
+The third line has an integer, taxPercent (the percentage of mealcost being added as tax).
+
+Output Format
+Print The total meal cost is totalCost dollars., where totalCost is the rounded integer result of the entire bill 
+(mealCost with added tax and tip).
+
+Sample Input
+
+12.00
+20
+8
+Sample Output
+
+The total meal cost is 15 dollars.
+*/
+
