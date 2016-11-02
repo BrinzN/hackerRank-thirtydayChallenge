12d8f6736a49359c81dbfbcff9c09a186131f0bc
BrinzN <svn.brindha@gmail.com>
2016-11-02 13:28:57 -0500

    Create DayFourCondnlStmts.java

---
+++ DayFourCondnlStmts.java
@@ -0,0 +1,43 @@
+/*
+Task 
+Given an integer, n , perform the following conditional actions:
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+Complete the stub code provided in your editor to print whether or not n is weird.
+
+Input Format
+A single line containing a positive integer, .
+
+Constraints
+1<=n<=100
+
+Output Format
+Print Weird if the number is weird; otherwise, print Not Weird.
+*/
+
+public class DayFourCondnlStmts{
+ public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        String ans="";
+
+        // if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
+        if(n%2==1){
+            ans = "Weird";
+        }
+        else{
+            if((n>=2 && n<=5) || n>20)
+            {
+                ans ="Not Weird" ;
+            }
+            else if(n>=6 && n<=20){
+                ans ="Weird" ;
+            }
+
+        }
+        System.out.println(ans);
+    }
+ }
