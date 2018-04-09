e43c96c3ad185b4795fca8c50bb475dc4cbe9340
BrinzN <23201151+BrinzN@users.noreply.github.com>
2018-04-09 13:14:23 +0530

    Create DayFourClassInstance.java

---
+++ DayFourClassInstance.java
@@ -0,0 +1,17 @@
+public class DayFourClassInstances {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int age = sc.nextInt();
+            DayFourPerson p = new DayFourPerson(age);
+            p.amIOld();
+            for (int j = 0; j < 3; j++) {
+                p.yearPasses();
+            }
+            p.amIOld();
+            System.out.println();
+        }
+        sc.close();
+    }
+}
