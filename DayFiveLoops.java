34fbd44839fda35025524fcf72df7ee33d2771b9
BrinzN <23201151+BrinzN@users.noreply.github.com>
2018-04-09 13:17:48 +0530

    Create DayFiveLoops.java

---
+++ DayFiveLoops.java
@@ -0,0 +1,12 @@
+public class DayFiveLoops {
+  public static void main(String[] args) {
+              Scanner in = new Scanner(System.in);
+              int N = in.nextInt();
+
+              if(N>=2 && N<=20){
+                  for(int i=1; i<=10; i++){
+                      System.out.println(N+" x "+i+" = "+ N*i);
+                  }
+              }
+          }
+  }
